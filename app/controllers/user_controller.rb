8726e98efd02fe580545896c56564e2ce92724f9
Benny Mei <meibenny@gmail.com>
2015-03-25 21:26:12 -0300

    flash message works - it is in the upper left hand corner. Needs CSS

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,8 @@
 class UserController < ApplicationController
   def create
 
-    unless params[:terms]
-      flash[:notice] = "Need to accept conditions"
-    else
-      flash[:notice] = 'yay'
-    end
-    redirect_to root_page_path
+    flash[:notice] = "thank you"
+    redirect_to index_page_path
 
   end
 
